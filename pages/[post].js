ed213377b349cd6261018e1e3f1162d8b6876955
behar lushaku <beharlushaku@behars-MBP.bbrouter>
2024-07-30 15:43:00 +0200

    e fundit

--- pages/[post].js
+++ pages/[post].js
@@ -8,18 +8,17 @@ const Post = (post) => {
     const [title, setTitle] = useState('');
     const [featuredImage, setFeaturedImage] = useState('');
 
-    setTitle(post.response.title.rendered);
-    setFeaturedImage(post.response.yoast_head_json.og_image[0].url);
-
+    // setTitle(post.response.title.rendered);
+    // setFeaturedImage(post.response.yoast_head_json.og_image[0].url);
     return (
     <div>
       <Head>
         <title>Chinesse</title>
-        <meta property="og:image" content={featuredImage} key="image" />
+        <meta property="og:image" content={post.response.yoast_head_json.og_image[0].url} key="image" />
       </Head>
       <main>
-        <h1>{title}</h1>
-        <img src={featuredImage}/>
+        <h1>{post.response.title.rendered}</h1>
+        <img src={post.response.yoast_head_json.og_image[0].url}/>
       </main>
     </div>
     )
@@ -28,11 +27,6 @@ const Post = (post) => {
 export default Post;
 
 Post.getInitialProps = async ({ req }) => {
-    let userIP
-    if (req) {
-      userIP = req.headers['x-real-ip'] || req.connection.remoteAddress
-    }
-
     
     const baseUrl = 'https://animalp4radise.com';
               let response = await fetch(`${baseUrl}/wp-json/wp/v2/posts/13554`)
